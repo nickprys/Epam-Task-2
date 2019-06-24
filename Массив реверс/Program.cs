973c498aba846e26e59fb9b1ee450533285632c2
nickprys <nickprys@gmail.com>
2019-06-24 23:10:25 +0300

    no message

---
+++ Массив реверс/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Массив_реверс
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] array = new int[4] { 9,8,5,1 };
+            int temp;
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+            
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Сумма четных if/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Сумма_четных_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первое число: ");
+            string namefirst = Console.ReadLine();
+            var numberfirst = int.Parse(namefirst);
+
+            Console.Write("Введите число: ");
+            string namesec = Console.ReadLine();
+            var numbersec = int.Parse(namesec);
+
+            int sum = 0;
+            for (int i = numberfirst; i <= numbersec; i++)
+            {
+                if (i % 2 == 0) 
+
+                sum += i;
+            }
+
+            Console.WriteLine("Сумма четных от {0} до {1} : {2}", namefirst, namesec, sum);
+            Console.ReadKey();
+        }
+    }
+}
